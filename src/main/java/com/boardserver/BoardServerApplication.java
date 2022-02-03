76f298e75e65f9703aa344c05831a40cccae52da
강보찬 <ebfks0301@gmail.com>
2022-02-03 14:31:42 +0900

    spring 실습을 위한 최초 커밋

---
+++ src/main/java/com/boardserver/BoardServerApplication.java
@@ -0,0 +1,18 @@
+package com.boardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(BoardServerApplication.class);
+		
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
+		
+	}
+
+}
